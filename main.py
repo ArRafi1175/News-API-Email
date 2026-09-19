2c5923650b9202229eaaad6b7a9c50c1f2869e7c
ArRafi1175 <arrafi1175@gmail.com>
2026-09-19 21:50:15 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#gets API key from .env
+#gets API key taken from the website from .env
 
 api_key = os.getenv("news_api_key")
 url = f"https://newsapi.org/v2/everything?q=tesla&from=2026-08-16&sortBy=publishedAt&apiKey={api_key}"
