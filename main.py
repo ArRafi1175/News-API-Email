5936298b818435734b25b056c3a8bea1a51c9214
ArRafi1175 <arrafi1175@gmail.com>
2026-09-20 11:48:33 +0100

    Access the news titles and descriptions

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#gets API key taken from the website from .env
+#gets API key taken from .env
 
 api_key = os.getenv("news_api_key")
-url = f"https://newsapi.org/v2/everything?q=tesla&from=2026-08-16&sortBy=publishedAt&apiKey={api_key}"
+url = f"https://newsapi.org/v2/everything?q=tesla&from=2026-08-20&sortBy=publishedAt&apiKey={api_key}"
 
+# Make request
 request = requests.get(url)
-content = request.text
-print(content)
+
+# Get a dictionary with Data
+content = request.json()
+
+# Access the article titles and descriptions
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
